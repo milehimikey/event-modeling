e489e39d59c13e6efaab6238ea128176c27277f1
mikey <milehimikey@gmail.com>
2025-03-22 13:39:52 -0600

    Start it

---
+++ src/main/kotlin/wtf/milehimikey/event_modeling/DemoApplication.kt
@@ -0,0 +1,11 @@
+package wtf.milehimikey.event_modeling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
---
+++ src/test/kotlin/wtf/milehimikey/event_modeling/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package wtf.milehimikey.event_modeling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
